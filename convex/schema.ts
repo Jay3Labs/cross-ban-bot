1ed62dee9685a1d957d703168ba080bb3d87371d
Matt Hatcher <matthatcher@pm.me>
2026-01-17 02:51:52 -0500

    initial convex setup

---
+++ convex/schema.ts
@@ -0,0 +1,14 @@
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
+
+export default defineSchema({
+  tracker_sessions: defineTable({
+    guild_id: v.string(),
+    user_id: v.string(),
+    session_id: v.string(),
+    start_epoch: v.optional(v.number()),
+    end_epoch: v.optional(v.number()),
+  })
+    .index('by_guild_id_and_user_id', ['guild_id', 'user_id'])
+    .index('by_session_id', ['session_id']),
+})
---
+++ convex/tracker.ts
@@ -0,0 +1,72 @@
+import { v } from 'convex/values'
+import { mutation, query } from './_generated/server'
+
+export const getTrackerSessions = query({
+  args: {
+    guild_id: v.string(),
+    user_id: v.string(),
+  },
+  async handler(ctx, args) {
+    return await ctx.db
+      .query('tracker_sessions')
+      .withIndex('by_guild_id_and_user_id', (q) => q.eq('guild_id', args.guild_id).eq('user_id', args.user_id))
+      .collect()
+  },
+})
+
+export const startTrackerSession = mutation({
+  args: {
+    guild_id: v.string(),
+    user_id: v.string(),
+    session_id: v.string(),
+    start_epoch: v.number(),
+  },
+  async handler(ctx, args) {
+    const exists = await ctx.db
+      .query('tracker_sessions')
+      .withIndex('by_session_id', (q) => q.eq('session_id', args.session_id))
+      .first()
+
+    if (exists) {
+      console.log(
+        `Session already exists for user. Guild: ${args.guild_id} User: ${args.user_id} Session: ${args.session_id}`,
+      )
+
+      return
+    }
+
+    await ctx.db.insert('tracker_sessions', {
+      guild_id: args.guild_id,
+      user_id: args.user_id,
+      session_id: args.session_id,
+      start_epoch: args.start_epoch,
+    })
+  },
+})
+
+export const endTrackerSession = mutation({
+  args: {
+    guild_id: v.string(),
+    user_id: v.string(),
+    session_id: v.string(),
+    end_epoch: v.number(),
+  },
+  async handler(ctx, args) {
+    const exists = await ctx.db
+      .query('tracker_sessions')
+      .withIndex('by_session_id', (q) => q.eq('session_id', args.session_id))
+      .first()
+
+    if (!exists) {
+      console.log(
+        `Session does not exist for user. Guild: ${args.guild_id} User: ${args.user_id} Session: ${args.session_id}`,
+      )
+
+      return
+    }
+
+    await ctx.db.patch('tracker_sessions', exists._id, {
+      end_epoch: args.end_epoch,
+    })
+  },
+})
